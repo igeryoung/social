2deb695efad949803000fe1d9d7ec27a72a235db
Shine <ShineChang@bitbucket.org>
2020-05-23 16:12:55 +0800

    s

--- app/src/main/java/com/example/social/PersonalInformationActivity.java
+++ app/src/main/java/com/example/social/PersonalInformationActivity.java
@@ -2,8 +2,10 @@ package com.example.social;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class PersonalInformationActivity extends AppCompatActivity {
 
@@ -11,9 +13,12 @@ public class PersonalInformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_information);
+
     }
 
     public void AddPhoto(View view) {
+
+        startSwipe();
     }
 
     public void Certain(View view) {
@@ -22,5 +27,10 @@ public class PersonalInformationActivity extends AppCompatActivity {
 
     public void Cencal(View view) {
         finish();
+    }
+
+    public void startSwipe(){
+        Intent next_page = new Intent(PersonalInformationActivity.this , TinderSwipe.class );
+        startActivity(next_page);
     }
 }
